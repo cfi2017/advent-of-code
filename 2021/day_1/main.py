8033c55facec54c70c1968c33237ee556b2929c1
Carlo Field <cfi@bluesky-it.ch>
2021-12-02 04:59:26 +0100

    update to 2021

---
+++ 2021/day_1/main.py
@@ -0,0 +1,13 @@
+from aocd.models import Puzzle
+from aocd import submit
+
+puzzle = Puzzle(year=2021, day=1)
+data = puzzle.input_data
+lines = list(map(int, data.splitlines()))
+
+result_a = sum([lines[i] < num for i, num in enumerate(lines[1:])])
+result_b = sum([sum(lines[i:i + 3]) < sum(lines[i + 1:i + 4]) for i, num in enumerate(lines[:-3])])
+
+submit(result_a, part="a", year=2021, day=1)
+submit(result_b, part="b", year=2021, day=1)
+
