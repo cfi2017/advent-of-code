9ea28267c52bb87a539d532e5a505d847ff01d99
Carlo Field <carlo@swiss.dev>
2023-12-07 13:15:37 +0100

    clean up a little

--- rust/aoc_rust/src/e2023/day06.rs
+++ rust/aoc_rust/src/e2023/day06.rs
@@ -1,3 +1,4 @@
+use std::ops::Mul;
 use num::ToPrimitive;
 use crate::aoc_boilerplate;
 
@@ -10,11 +11,15 @@ use crate::aoc_boilerplate;
 /// t1 = (t_total + sqrt(t_total^2 - 4 * d_travelled)) / 2
 /// t2 = (t_total - sqrt(t_total^2 - 4 * d_travelled)) / 2
 
-fn solutions(time: i64, distance: i64) -> i64 {
-    let time = time as f64;
-    let distance = (distance + 1) as f64;
-    let t1 = (time + (time * time - 4.0 * distance).sqrt()) / 2.0;
-    let t2 = (time - (time * time - 4.0 * distance).sqrt()) / 2.0;
+fn solution_tuples((t, d): (i64, i64)) -> i64 {
+    solution(t, d)
+}
+
+fn solution(t: i64, d: i64) -> i64 {
+    let t = t as f64;
+    let d = (d + 1) as f64;
+    let t1 = (t + (t * t - 4.0 * d).sqrt()) / 2.0;
+    let t2 = (t - (t * t - 4.0 * d).sqrt()) / 2.0;
     let t1 = t1.floor().to_i64().unwrap();
     let t2 = t2.ceil().to_i64().unwrap();
     t1 - t2 + 1
@@ -22,16 +27,13 @@ fn solutions(time: i64, distance: i64) -> i64 {
 
 aoc_boilerplate!(2023, 6, sanitize_a, sanitize_b, solve_a, solve_b);
 
+/// INPUT SANITIZATION
+
 pub fn sanitize_a(input: &str) -> Vec<(i64, i64)> {
     let mut lines = input.lines();
     let times = lines.next().unwrap().split(':').nth(1).unwrap();
     let distances = lines.next().unwrap().split(':').nth(1).unwrap();
     times.split(' ').filter(|x| !x.is_empty()).zip(distances.split(' ').filter(|x| !x.is_empty())).map(|(t, d)| (t.parse::<i64>().unwrap(), d.parse::<i64>().unwrap())).collect()
-}
-
-pub fn solve_a(races: Vec<(i64, i64)>) -> i64 {
-    races.iter().map(|(t, d)| solutions(*t, *d))
-        .reduce(|a, b| a * b).unwrap()
 }
 
 pub fn sanitize_b(input: &str) -> (i64, i64) {
@@ -41,8 +43,18 @@ pub fn sanitize_b(input: &str) -> (i64, i64) {
     (time, distance)
 }
 
+/// SOLVING CODE
+
+pub fn solve_a(races: Vec<(i64, i64)>) -> i64 {
+    races.iter()
+        .copied()
+        .map(solution_tuples)
+        .reduce(i64::mul)
+        .unwrap()
+}
+
 pub fn solve_b((time, distance): (i64, i64)) -> i64 {
-    solutions(time, distance)
+    solution(time, distance)
 }
 
 #[cfg(test)]
